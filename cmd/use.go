8df0f4c49ce89e7cb1312490a07b0e676322c9a0
Mukul Sharma <mymukul112@gmail.com>
2023-10-31 23:13:51 +0530

    Support for reading php version from file .phpversion

--- cmd/use.go
+++ cmd/use.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/mukul98s/boggart/helper"
 	"github.com/spf13/cobra"
+	"os"
 	"os/exec"
 )
 
@@ -11,52 +12,58 @@ import (
 var useCmd = &cobra.Command{
 	Use:     "use",
 	Short:   "Switch php version",
-	Long:    `Switch php version by mentioning version number`,
+	Long:    `Switch php version by mentioning version number or by mentioning php version in .phpversion file`,
 	Example: "boggart use 8.2",
 	Args:    cobra.MaximumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
-			err := cmd.Help()
-			if err != nil {
-				return
-			}
-			return
-		}
-		phpVersionNumber := helper.RemoveWhitespace(args[0])
+	Run:     runUseCommand,
+}
 
-		if !helper.IsValidPhpVersion(phpVersionNumber) {
-			fmt.Printf("Please Enter a valid version number\n\n")
-			err := cmd.Help()
-			if err != nil {
-				return
-			}
-			return
-		}
+func runUseCommand(cmd *cobra.Command, args []string) {
+	var phpVersionNumber string
 
-		phpVersionCheck := fmt.Sprintf("php%v", phpVersionNumber)
-		command := exec.Command("which", phpVersionCheck)
-		output, err := command.Output()
-		if err != nil || string(output) == "" {
-			fmt.Printf("PHP v%s is not installed on your system\n", phpVersionNumber)
-			return
-		}
+	file, _ := os.ReadFile(".phpversion")
+	if len(file) > 0 {
+		phpVersionNumber = helper.RemoveWhitespace(string(file))
+	}
 
-		phpPath := string(output)
-		phpPath = helper.RemoveWhitespace(phpPath)
+	if len(args) == 0 && phpVersionNumber == "" {
+		_ = cmd.Help()
+		return
+	}
 
-		changePhpVersionCommand := exec.Command("sudo", "update-alternatives", "--set", "php", phpPath)
-		output, err = changePhpVersionCommand.Output()
-		if err != nil {
-			fmt.Printf("Unable to change PHP to v%s\n", phpVersionNumber)
-			return
-		}
-		if string(output) == "" {
-			fmt.Printf("Already using using PHP v%s\n", phpVersionNumber)
-			return
-		}
+	if len(args) > 0 {
+		phpVersionNumber = helper.RemoveWhitespace(args[0])
+	}
 
-		fmt.Printf("Now using PHP version v%s\n", phpVersionNumber)
-	},
+	if !helper.IsValidPhpVersion(phpVersionNumber) {
+		fmt.Printf("Please Enter a valid version number\n\n")
+		_ = cmd.Help()
+		return
+	}
+
+	phpVersionCheck := fmt.Sprintf("php%v", phpVersionNumber)
+	command := exec.Command("which", phpVersionCheck)
+	output, err := command.Output()
+	if err != nil || string(output) == "" {
+		fmt.Printf("PHP v%s is not installed on your system\n", phpVersionNumber)
+		return
+	}
+
+	phpPath := string(output)
+	phpPath = helper.RemoveWhitespace(phpPath)
+
+	changePhpVersionCommand := exec.Command("sudo", "update-alternatives", "--set", "php", phpPath)
+	output, err = changePhpVersionCommand.Output()
+	if err != nil {
+		fmt.Printf("Unable to change PHP to v%s\n", phpVersionNumber)
+		return
+	}
+	if string(output) == "" {
+		fmt.Printf("Already using using PHP v%s\n", phpVersionNumber)
+		return
+	}
+
+	fmt.Printf("Now using PHP version v%s\n", phpVersionNumber)
 }
 
 func init() {
